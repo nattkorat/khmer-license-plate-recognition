5f1a7982e9789dda381835c07eedf8479fd2019a
natt <natt.korat@cadt.edu.kh>
2023-08-28 14:41:13 +0700

    add feature from dev

--- app.py
+++ app.py
@@ -58,114 +58,11 @@ def upload_file():
         x, y, x1, y1 = r
 
         plate = image[y:y1, x:x1].copy()
-<<<<<<< HEAD
-        # cv2.imwrite('plate.jpg', plate)
-
-        place, bbox = extract.get_info(plate)
-
-        pl_img = plate.copy()
-        if len(bbox) >= 4:
-            cv2.rectangle(pl_img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,255,0), 2)
-
-        # cv2.imwrite('place_det.jpg', pl_img)
-
-        image = plotting.plotting(image, r, place)
-        
-
-        serials = extract.roi(plate, 1)
-        if len(serials) > 0: 
-            serials = serials[0]
-            a, b, a1, b1 = serials
-
-            # cv2.imwrite('serial.jpg', plate[b:b1, a:a1])
-
-            orgin = reader.readtext(plate[b:b1, a:a1])
-
-            or_txt = ''
-            or_conf = 0
-            for inf in orgin:
-                print(inf)
-                if inf[-1] >= 0.3:
-                    or_conf += inf[-1]
-                    or_txt = inf[1]
-
-            pre_img = image_pre.pre_process(plate[b:b1, a:a1])
-            sh_img = image_pre.img_shapen(pre_img)
-            processed = reader.readtext(sh_img)
-
-            pro_txt = ''
-            pro_conf = 0
-
-            for inf in processed:
-                print(inf)
-                if inf[-1] >= 0.3:
-                    pro_txt += inf[1]
-                    pro_conf = inf[-1]
-
-            if or_conf > pro_conf:
-                conf = or_conf
-                serial_val = or_txt
-            else:
-                conf = pro_conf
-                serial_val = pro_txt
-            
-            serial_val = post_process.char_map(serial_val, place) # apply post process method
-
-            image = plotting.plotting(image, r, place + ' ' + serial_val)
-
-            
-        else:
-            if len(bbox) > 0:
-                plate = cv2.rectangle(plate, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255,255,255), -1)
-            
-            orgin = reader.readtext(plate)
-
-            or_txt = ''
-            or_conf = 0
-            for inf in orgin:
-                print(inf)
-                if inf[-1] >= 0.3:
-                    or_conf += inf[-1]
-                    or_txt = inf[1]
-
-            pre_img = image_pre.pre_process(plate)
-            sh_img = image_pre.img_shapen(pre_img)
-            processed = reader.readtext(sh_img)
-
-            pro_txt = ''
-            pro_conf = 0
-
-            for inf in processed:
-                print(inf)
-                if inf[-1] >= 0.3:
-                    pro_txt += inf[1]
-                    pro_conf = inf[-1]
-
-            if or_conf > pro_conf:
-                conf = or_conf
-                serial_val = or_txt
-            else:
-                conf = pro_conf
-                serial_val = pro_txt
-
-            serial_val = post_process.char_map(serial_val, place) # apply post process method
-            image = plotting.plotting(image, r, place + ' ' + serial_val)
-
-        result.append({
-            "plate_roi": r,
-            "serial_roi": serials,
-            "plate_name": place,
-            "serial_value": serial_val,
-            "conf": conf,
-            "datetime": str(datetime.now(time_zone))
-        })
-=======
         cv2.imwrite('plate.jpg', plate)
         data = processs_OCR(plate)
         image = plotting.plotting(image, r, data['plate_name'] + ' ' + data['serial_value'] + ' ' + f'{data["conf"]:.3f}')
 
         result.append(data)
->>>>>>> dev
 
     # Convert the CV2 image to a binary format (PNG or JPEG)
     _, buffer = cv2.imencode('.jpg', image)
